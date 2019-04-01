91364b67cb1814c317f0e2bd63fedf20ab7b1fd3
Googler <noreply@google.com>
2019-04-01 16:00:27 -0700

    END_PUBLIC RSL: Added new test definition for native RSL
    PiperOrigin-RevId: 241418311

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeAllocationRegistry.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeAllocationRegistry.java
@@ -1,21 +1,51 @@
 package org.robolectric.shadows;
 
+
 import static android.os.Build.VERSION_CODES.N;
+import static org.robolectric.shadow.api.Shadow.directlyOn;
 
 import libcore.util.NativeAllocationRegistry;
+import libcore.util.NativeAllocationRegistry.Allocator;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
+import org.robolectric.annotation.RealObject;
+
 
 @Implements(value = NativeAllocationRegistry.class, minSdk = N, isInAndroidSdk = false, looseSignatures = true)
 public class ShadowNativeAllocationRegistry {
 
-  @Implementation
-  protected Runnable registerNativeAllocation(Object referent, Object allocator) {
-    return null;
+  @RealObject
+  protected NativeAllocationRegistry realNativeAllocationRegistry;
+
+  private static boolean hasNativeImplementation(Class<?> clazz) {
+    try {
+      boolean nativesRegistered = clazz
+          .getDeclaredField("$$robo_native_registered_flag$$").getBoolean(null);
+      return nativesRegistered;
+    } catch (NoSuchFieldException ignored) {
+      //Probably not instrumented
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException("ROBO_NATIVE_FLAG should be public", e);
+    }
+    return false;
   }
 
   @Implementation
-  protected Runnable registerNativeAllocation(Object referent, long nativePtr) {
-    return null;
+  protected Runnable registerNativeAllocation(Object referent, Object allocator) {
+    if (hasNativeImplementation(referent.getClass())) {
+      return directlyOn(realNativeAllocationRegistry, NativeAllocationRegistry.class).
+          registerNativeAllocation(referent, (Allocator) allocator);
+    }
+    return () -> {};
+  }
+
+
+  @Implementation
+  public Runnable registerNativeAllocation(Object referent, long nativePtr) {
+    if (hasNativeImplementation(referent.getClass())) {
+      return directlyOn(realNativeAllocationRegistry, NativeAllocationRegistry.class).
+          registerNativeAllocation(referent, nativePtr);
+    }
+    return () -> {};
   }
 }
